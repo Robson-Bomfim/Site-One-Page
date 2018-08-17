14f0c1d445b82791b6abf75324e28005169a4c41
Robson-Bomfim <31391898+Robson-Bomfim@users.noreply.github.com>
2018-08-16 21:00:56 -0300

    commit ajuste navbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+
+ /* SCRIPT JQUERY PARA TROCAR DE COR NAVBAR */
+$(document).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+        $('.navbar').css("background-color", "#68627a");
+        $('.navbar').css("transition", "all .60s ease-in-out");
+        $( ".navbar" ).addClass( "bg-topo" );
+    } else {
+        $('.navbar').css("background-color", "transparent")
+    }
+});
+        
 (function($){
 
 	"use strict";
