426ec00b9d4569bdf6a8db21b1b5e9ee28467c9f
Robson-Bomfim <31391898+Robson-Bomfim@users.noreply.github.com>
2018-08-16 22:38:28 -0300

    commit para adicionar o move to top

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
 
  /* SCRIPT JQUERY PARA TROCAR DE COR NAVBAR */
 $(document).scroll(function () {
-    if ($(this).scrollTop() > 50) {
+
+    if ($(this).scrollTop() > 80) {
+
         $('.navbar').css("background-color", "#68627a");
         $('.navbar').css("transition", "all .60s ease-in-out");
-        $( ".navbar" ).addClass( "bg-topo" );
+        $(".navbar").addClass( "bg-topo" );
+        $(".fa-chevron-up").addClass( "fixed-bottom" );
+
     } else {
-        $('.navbar').css("background-color", "transparent")
+        $('.navbar').css("background-color", "transparent");
+        $(".fa-chevron-up").removeClass( "fixed-bottom" );
     }
+
 });
+
+// Faz animação para aparecer e sumir
+if ($(this).scrollTop() > 80) {
+
+        $(".fa-chevron-up").addClass( "fixed-bottom" );
+
+    } else {
+        
+        $(".fa-chevron-up").removeClass( "fixed-bottom" );
+    }
+
+        
+// Faz animação para subir
+$(".fa-chevron-up").click(function(event) {
+
+    event.preventDefault();
+    jQuery('html, body').animate({scrollTop: 0}, 500);
+
+});
+
+
         
 (function($){
 
